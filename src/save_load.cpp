b7ec3874e9c78d3b0f375f2d70e164ef5ca41ac5
AntipinaVeronika <va.antipina@gmail.com>
2024-12-04 23:49:05 +0300

    Manager s\l

---
+++ src/save_load.cpp
@@ -0,0 +1,63 @@
+#include "keeper.cpp"
+
+class SaveLoad{
+private:
+    std::fstream strict;
+
+    void
+    insecure_save( GameState* state ){
+        SaveKeeper keep = SaveKeeper(state);
+        // keep.save();
+    }
+
+    void
+    prepare(){
+        SecretAgent agent_007;
+        if( strict.is_open()){
+            int control_sum = agent_007.getSum();
+            int current_sum = 0;
+            strict >> current_sum;
+            if( current_sum != control_sum ){
+                throw CheatingAlert();
+            }
+        }else{
+            exit(0);
+        }
+    }
+
+public:
+    SaveLoad(){
+        strict.open("check.txt");
+        if( !strict.is_open() )
+            throw FileDoesNotExist();
+    }
+    ~SaveLoad(){
+        strict.close();
+    }
+
+    void
+    save( GameState* state ){
+        insecure_save( state );
+
+        SecretAgent agent_007;
+        if( strict.is_open()){
+            int control_sum = agent_007.getSum();
+            strict << control_sum;
+        }else{
+            exit(0);
+        }
+    }
+
+    void
+    load( GameState* state ){
+        try{
+            prepare();
+        }
+        catch( CheatingAlert& e ){
+            std::cout << e.what();
+            exit(0);
+        }
+        LoadKeeper keep = LoadKeeper(state);
+        //keep.load();
+    }
+};
